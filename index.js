d891572fa285bf0bb8b5d2b27c90c4f2d6678750
Lara Sanders <larasanders@gmail.com>
2018-08-02 21:46:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,20 @@ function setThisWithCall(fn, thisValue, arg) {
   return fn.call(thisValue, arg)
 }
 
+<<<<<<< HEAD
 function setThisWithApply(fn, thisValue, arg) {
   return fn.apply(thisValue, arg)
+=======
+function setThisWithApply(fn, thisValue, args) {
+  return fn.apply(thisValue, [arg])
+>>>>>>> 230def3c5f1ac6f2e7de886eabeec88b9533a3a3
 }
 
 function returnNewFunctionOf(functionToBeCopied, thisValue) {
   const newFunction = functionToBeCopied.bind(thisValue)
   return newFunction
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 230def3c5f1ac6f2e7de886eabeec88b9533a3a3
