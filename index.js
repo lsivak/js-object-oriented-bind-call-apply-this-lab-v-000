2baa2035b38eacaa37406c50a9b6fb8fd66fba6e
Lara Sanders <larasanders@gmail.com>
2018-08-02 21:53:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 function justInvoke(fn) {
 return fn();
+
 }
 
 function setThisWithCall(fn, thisValue, arg) {
   return fn.call(thisValue, arg)
 }
 
+
 function setThisWithApply(fn, thisValue, arg) {
   return fn.apply(thisValue, arg)
+
+
+function setThisWithApply(fn, thisValue, args) {
+  return fn.apply(thisValue, [arg])
+}
 }
 
 function returnNewFunctionOf(functionToBeCopied, thisValue) {
   const newFunction = functionToBeCopied.bind(thisValue)
   return newFunction
+
 }
